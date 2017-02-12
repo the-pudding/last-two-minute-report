86b008f919b6b3801d8259d3339469da77daa877
russellgoldenberg <russellgoldenberg@gmail.com>
2017-02-11 21:32:14 -0500

    sort

--- web/src/js/graphic-calls.js
+++ web/src/js/graphic-calls.js
@@ -4,6 +4,16 @@ import colors from './colors'
 
 const graphic = d3.select('.graphic__calls')
 const chart = graphic.select('.graphic__chart')
+let colorsIndex
+let colorsLight
+let scale
+let callData
+
+function lighten(hex) {
+	const color = d3.color(hex)
+	color.opacity = 0.3
+	return color.toString()
+}
 
 function formatPercent(num) {
 	return d3.format('.1%')(num)
@@ -21,53 +31,118 @@ function cleanData(row) {
 	const inc = +row.count_inc
 	const total = cc + cnc + ic + inc
 	const total_infraction = total - cnc
-	const rate_correct = (cc) / total_infraction
-	const rate_incorrect = 1 - rate_correct
-	return { call, cc, cnc, ic, inc, total, total_infraction, rate_correct, rate_incorrect }
+	const rate = cc / total_infraction
+	// const rate_incorrect = 1 - rate_correct
+	return { call, cc, cnc, ic, inc, total, total_infraction, rate }
 }
 
-function createTable(data) {
-	const callData = data
-		.filter(d => d.total_infraction > 20)
-		.sort((a, b) => d3.descending(a.rate_correct, b.rate_correct))
+function createScale(data) {
+	return {
+		cc: d3.scaleQuantile().domain(data.map(d => d.cc)),
+		ic: d3.scaleQuantile().domain(data.map(d => d.ic)),
+		inc: d3.scaleQuantile().domain(data.map(d => d.inc)),
+		rate: d3.scaleThreshold().domain([0.2, 0.4, 0.6, 0.8]),
+	}
+}
 
-	const scale = d3.scaleThreshold()
+function handleColumnClick(d) {
+	const col = d3.select(this).attr('data-col')
+	updateTable(col)
+}
 
-	scale
-		.domain([0.2, 0.4, 0.6, 0.8])
-		.range(colors.diverging)
-
+function createTable() {
 	const tableEnter = chart.append('table')
 
 	const headEnter = tableEnter.append('thead')
 		.append('tr')
 
-	headEnter.append('th').text('Call').classed('text', true)
-	headEnter.append('th').text('CC').classed('number', true)
-	headEnter.append('th').text('IC').classed('number', true)
-	headEnter.append('th').text('INC').classed('number', true)
-	headEnter.append('th').text('Rate correct').classed('number', true)
+	headEnter.append('th')
+		.text('Call')
+		.classed('text', true)
+
+	headEnter.append('th')
+		.attr('data-col', 'cc')
+		.text('CC')
+		.classed('number', true)
+		.on('click', handleColumnClick)
+
+	headEnter.append('th')
+		.attr('data-col', 'ic')
+		.text('IC')
+		.classed('number', true)
+		.on('click', handleColumnClick)
+
+	headEnter.append('th')
+		.attr('data-col', 'inc')
+		.text('INC')
+		.classed('number', true)
+		.on('click', handleColumnClick)
+
+	headEnter.append('th')
+		.attr('data-col', 'rate')
+		.text('Rate correct')
+		.classed('number', true)
+		.on('click', handleColumnClick)
 
 	const bodyEnter = tableEnter.append('tbody')
 
 	const trEnter = bodyEnter.selectAll('tr')
-		.data(callData)
+		.data(callData, (d, i) => i)
 		.enter().append('tr')
 
-	trEnter.style('background-color', d => scale(d.rate_correct))
+	// trEnter.style('background-color', d => scale(d.rate))
 
 	trEnter.append('td').text(d => d.call).classed('text', true)
-	trEnter.append('td').text(d => formatComma(d.cc)).classed('number', true)
-	trEnter.append('td').text(d => formatComma(d.ic)).classed('number', true)
-	trEnter.append('td').text(d => formatComma(d.inc)).classed('number', true)
-	trEnter.append('td').text(d => formatPercent(d.rate_correct)).classed('number', true)
 
+	trEnter.append('td')
+		.attr('class', 'td-cc')
+		.classed('number', true)
+
+	trEnter.append('td')
+		.attr('class', 'td-ic')
+		.classed('number', true)
+
+	trEnter.append('td')
+		.attr('class', 'td-inc')
+		.classed('number', true)
+
+	trEnter.append('td')
+		.attr('class', 'td-rate')
+		.classed('number', true)
 }
 
+function updateTable(col) {
+	const sortedData = callData.sort((a, b) => d3.descending(a[col], b[col]))
+
+	const tr = chart.selectAll('tbody tr')
+		.data(sortedData, (d, i) => i)
+
+	tr.select('.td-cc').text(d => formatComma(d.cc))
+	tr.select('.td-ic').text(d => formatComma(d.ic))
+	tr.select('.td-inc').text(d => formatComma(d.inc))
+	tr.select('.td-rate').text(d => formatPercent(d.rate))
+
+	Object.keys(scale).forEach((key) => {
+		const range = col === key ? colors.diverging : colorsLight
+		scale[key].range(range)
+		chart.selectAll(`.td-${key}`)
+			.style('background-color', d => scale[key](d[key]))
+	})
+}
+
+function prepareData(data) {
+	return data.filter(d => d.total_infraction >= 20)
+}
 function init() {
+	colorsIndex = colors.diverging.map((d, i) => i)
+	colorsLight = colors.diverging.map(lighten)
+
 	d3.csv('assets/data/web_calls.csv', cleanData, (err,  data) => {
 		if (err) console.error(err)
-		createTable(data)
+		callData = prepareData(data)
+		scale = createScale(callData)
+		createTable()
+		updateTable('rate')
 	})
 }
 
