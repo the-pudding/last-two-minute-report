1687f60ae08366f7f90c26f098b8d2da1ad1b88c
russellgoldenberg <russellgoldenberg@gmail.com>
2017-02-15 14:36:48 -0500

    typos

--- web/src/js/utils/dom.js
+++ web/src/js/utils/dom.js
@@ -28,11 +28,14 @@ function hasClass(el, className) {
 	return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className)
 }
 
-function jumpTo(el) {
+function jumpTo(el, offset) {
+	offset = offset || 0
 	const top = el.getBoundingClientRect().top
-	document.body.scrollTop ?
-	document.body.scrollTop += top :
-	document.documentElement.scrollTop += top
+	const dist = top + offset
+
+	!isNaN(document.body.scrollTop) ?
+	document.body.scrollTop += dist :
+	document.documentElement.scrollTop += dist
 }
 
 
